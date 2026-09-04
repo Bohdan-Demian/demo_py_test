a8b976670285a24261185040c732a74ccce53785
Bohdan <2manmainmail@gmail.com>
2026-09-04 10:19:42 +0200

    Update conftest.py

--- pyspark_test/tests/conftest.py
+++ pyspark_test/tests/conftest.py
@@ -1,15 +1,29 @@
+import os
 import pytest
 from pyspark.sql import SparkSession
 
 
 @pytest.fixture(scope="session")
 def spark():
-    session = (
-        SparkSession.builder.appName("pyspark-ecommerce-quality-tests")
-        .master("local[2]")
-        .config("spark.sql.shuffle.partitions", "2")
-        .getOrCreate()
-    )
-    yield session
-    session.stop()
 
+    if os.getenv("DATABRICKS_RUNTIME_VERSION"):
+        # Databricks
+        session = SparkSession.getActiveSession()
+
+        if session is None:
+            session = SparkSession.builder.getOrCreate()
+
+        yield session
+
+    else:
+        # Local
+        session = (
+            SparkSession.builder
+            .appName("pyspark-ecommerce-quality-tests")
+            .master("local[2]")
+            .config("spark.sql.shuffle.partitions", "2")
+            .getOrCreate()
+        )
+
+        yield session
+        session.stop()
